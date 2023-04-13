af69146fdbe67470c56d9b42092f2944c6605d2a
rempa359 <rempa@seznam.cz>
2023-04-13 11:43:46 +0200

    Verze aplikace 3.0. Použita vylepšená varianta JSpinner. Navíc upravena velikost polí.

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -20,6 +20,7 @@ public class Aplikace extends JFrame {
     private JTextField hlavyField;
     private JTextField nohyField;
     private JSpinner kraliciSpinner;
+
     private JSpinner husySpinner;
 
     private JButton vypocitatButton;
@@ -30,7 +31,7 @@ public class Aplikace extends JFrame {
     }
 
     public Aplikace() throws HeadlessException {
-        super("Farmářka Iva 2.0");
+        super("Farmářka Iva 3.0");
         this.init();
     }
 
@@ -43,27 +44,34 @@ public class Aplikace extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setIconImage(new ImageIcon(Aplikace.class.getResource("czechitas-icon.png")).getImage());
         setLayout(new MigLayout("wrap 2", "[right]rel[50:120:150,grow,fill]"));
-        setMinimumSize(new Dimension(250, 200));
+        setMinimumSize(new Dimension(300, 250));
 
-       // husyField = new JTextField();
-        husySpinner = new JSpinner();
+        // husyField = new JTextField();
+        SpinnerNumberModel husyModel = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
+        husySpinner = new JSpinner(husyModel);
         //husyField.setHorizontalAlignment(JTextField.TRAILING);
         husyLabel = new JLabel("Husy");
         husyLabel.setDisplayedMnemonic('H');
         husyLabel.setLabelFor(husySpinner);
         add(husyLabel);
         add(husySpinner, "span");
+        husySpinner.setPreferredSize(new Dimension(175, 25));
+        husySpinner.setMinimumSize(new Dimension(175, 25));
+        husySpinner.setMaximumSize(new Dimension(175, 25));
 
         //kraliciField = new JTextField();
-        kraliciSpinner = new JSpinner();
+        SpinnerNumberModel kraliciModel = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
+        kraliciSpinner = new JSpinner(kraliciModel);
 
-       //kraliciField.setHorizontalAlignment(JTextField.TRAILING);
+        //kraliciField.setHorizontalAlignment(JTextField.TRAILING);
         kraliciLabel = new JLabel("Králíci");
         kraliciLabel.setDisplayedMnemonic('K');
-       kraliciLabel.setLabelFor(kraliciSpinner);
+        kraliciLabel.setLabelFor(kraliciSpinner);
         add(kraliciLabel);
         add(kraliciSpinner, "span");
-
+        kraliciSpinner.setPreferredSize(new Dimension(175, 25));
+        kraliciSpinner.setMinimumSize(new Dimension(175, 25));
+        kraliciSpinner.setMaximumSize(new Dimension(175, 25));
         add(createButtonBar(), "span");
 
         pack();
@@ -79,6 +87,9 @@ public class Aplikace extends JFrame {
         hlavyLabel.setLabelFor(hlavyField);
         add(hlavyLabel);
         add(hlavyField, "span");
+        hlavyField.setPreferredSize(new Dimension(175, 25));
+        hlavyField.setMinimumSize(new Dimension(175, 25));
+        hlavyField.setMaximumSize(new Dimension(175, 25));
 
         nohyField = new JTextField();
         nohyField.setHorizontalAlignment(JTextField.TRAILING);
@@ -87,6 +98,9 @@ public class Aplikace extends JFrame {
         nohyLabel.setLabelFor(nohyField);
         add(nohyLabel);
         add(nohyField, "span");
+        nohyField.setPreferredSize(new Dimension(175, 25));
+        nohyField.setMinimumSize(new Dimension(175, 25));
+        nohyField.setMaximumSize(new Dimension(175, 25));
 
     }
 
@@ -132,4 +146,8 @@ public class Aplikace extends JFrame {
 
         int pocetHlav = pocetKraliku + pocetHus;
         hlavyField.setText(String.valueOf(pocetHlav));
-}}
+    }
+}
+
+
+
